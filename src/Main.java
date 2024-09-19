92563dec9d188baaaf4d508ed241aa2282edc31f
steph <stephenbenny050@gmail.com>
2024-09-19 14:40:39 -0400

    Completed Task 2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double winterCost = 0;
+        double springCost = 0;
+        double summerCost = 0;
+        double fallCost = 0;
+        final double yearlyCost = 0;
+
+        yearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("The cost in maintenance per year is " + yearlyCost);
+
+
+
+    }
+}
